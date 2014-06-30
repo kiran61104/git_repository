653864a16ad4976162c688cfca27327db47fa3f3
Kiran <coolguy.kiran19@gmail.com>
2014-06-30 22:49:28 +0530

    1st Commit

---
+++ TestProject/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	/**
+	 * Sample Main Method
+	 * */
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Start Coding");
+		Test test = new Test();
+		test.initGit();
+		
+		
+	}
+	
+	public void initGit(){
+		System.out.println("Started using Git ");
+	}
+
+}
